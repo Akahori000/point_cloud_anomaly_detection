cf1d0053b3a634d3d7539b9ea008289941f8763e
Akahori000 <shizukaa2j@gmail.com>
2022-11-08 19:01:22 +0000

    setting memo

---
+++ setting.sh
@@ -0,0 +1,14 @@
+
+conda env create -f=emd_env.yml
+
+conda activate emd_env
+
+pip install addict==2.4.0 dataclasses==0.6 efficientnet-pytorch==0.7.0 future==0.18.2 torch==1.7.0 typing-extensions==3.7.4.3 wandb
+
+cd libs/emd
+
+python setup.py install
+
+cd ../../
+
+python train.py ./config/sample_objset3.yaml
